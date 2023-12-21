4846ac9ef22a7f50589f3736728eaf277d83d747
Jared Elliott <bootcamp@jaredclt.com>
2023-12-21 15:48:50 -0500

    Added handleSave function, created btnSave event listener.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
+//Global Variables
+const schedulerName = 'work-day-scheduler'; //Used for the writeLocalStorage and readLocalStorage functions -- object name
+
 $(function () { // Call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
- 
- 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,15 +20,47 @@ $(function () { // Call to jQuery to ensure that the code isn't run until the br
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  // buttons have aria-label="save" in common
+  let btnSave = $('[aria-label="save"]');
+    btnSave.on("click", handleSave);
   
   // Display the current date in the header of the page.
   $('#currentDay').text(dayjs().format('dddd, MMMM DD'));
-  
+
 });
 
+//Functions to be called inside
 
 function getCurrentHour() {
   //Used to get the current hour
   console.log(dayjs().format('H'));
+}
+
+function readLocalStorage(storageItem) {
+  //Function to read the local storage with passed object name
+  let tempStorage = JSON.parse(localStorage.getItem(storageItem));
+  //Debug testing for if it's an object/array
+  console.log("readLocalStorage | Not an Object: " + !tempStorage);
+
+  if(!tempStorage) { tempStorage = []; localStorage.setItem(storageItem, JSON.stringify(tempStorage)); } 
+  console.log("readLocalStorage | " + JSON.stringify(tempStorage));
+  //Function returns the local storage object
+  return tempStorage;
+}
+
+function writeLocalStorage(storageItem, storageObject) {
+  //Function to write to LocalStorage
+  var currObject = readLocalStorage(lsName);
+
+  if(typeof storageObject !== 'object') { console.log("writeLocalStorage: Invalid type submitted."); return }
+  currObject.push(storageObject)
+
+  console.log('writeLocalStorage | ' + storageItem);
+  console.log('writeLocalStorage | ' + JSON.stringify(storageObject));
+
+  localStorage.setItem(storageItem, JSON.stringify(currObject))
+}
+
+function handleSave(event) {
+  console.log(event.target);
 }
